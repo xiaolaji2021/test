6316f35a2583d8681527248ae3f37e750dca0ddd
xiaolaji2021 <89297634+xiaolaji2021@users.noreply.github.com>
2021-08-26 13:55:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git OAF https://github.com/destan19/OpenAppFilter' feeds.conf.default
